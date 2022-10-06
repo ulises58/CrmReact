24876d90cb086632f6135d55c0e02045d91324a7
Ulises Sandoval <ulises.sandoval@uabc.edu.mx>
2022-10-05 18:36:01 -0700

    Creando el Router y definiendo primeras rutas

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>inicio</h1>,
+  },
+  {
+    path: "/test",
+    element: <h1>test</h1>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
