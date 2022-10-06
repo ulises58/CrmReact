c962a557a5bf250ee590d3bc441a9cc86960fc97
Ulises Sandoval <ulises.sandoval@uabc.edu.mx>
2022-10-06 11:41:38 -0700

    Navegar entre Componentes con Link

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 export const Layout = () => {
   return (
@@ -7,6 +7,20 @@ export const Layout = () => {
         <h2 className="text-4xl font-black text-center text-white">
           CRM - Clinte
         </h2>
+        <nav className="mt-10">
+          <Link
+            className="text-2xl block hover:text-blue-300 text-white"
+            to="/"
+          >
+            Clientes
+          </Link>
+          <Link
+            className="text-2xl block hover:text-blue-300 text-white"
+            to="/clientes/nuevo"
+          >
+            Nuevos
+          </Link>
+        </nav>
       </aside>
       <main className="md:w-3/4 p-10 md:h-screen overflow-scroll">
         <Outlet />
