1e396182ea23cf90508c9721e77903e702971181
Ulises Sandoval <ulises.sandoval@uabc.edu.mx>
2022-10-07 10:21:20 -0700

    Agregando el Formulario

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import Formulario from "../components/Formulario";
 
 export const NuevoCliente = () => {
   const navigate = useNavigate();
@@ -15,8 +16,15 @@ export const NuevoCliente = () => {
           VOLVER
         </button>
       </div>
-      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10">
-        <p>Formulario aqui</p>
+      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
+        <form>
+          <Formulario />
+          <input
+            type="submit"
+            className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg"
+            value="Registrar Cliente"
+          />
+        </form>
       </div>
     </>
   );
