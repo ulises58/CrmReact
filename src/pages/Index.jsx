7ccdfcde253faf5195456c2981a1b8c1a43bc1d2
Ulises Sandoval <ulises.sandoval@uabc.edu.mx>
2022-10-07 09:17:10 -0700

    Iterando sobre Clientes y Mostrando la información

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from "react-router-dom";
+import Cliente from "../components/Cliente";
 
 export function loader() {
   const clientes = [
@@ -42,12 +43,30 @@ export function loader() {
 }
 
 export const Index = () => {
-  const datos = useLoaderData();
-  console.log(datos);
+  const clientes = useLoaderData();
+  console.log(clientes);
   return (
     <>
       <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
       <p className="mt-3"> Administra tus Clientes</p>
+      {clientes.length ? (
+        <table className="w-full bg-white shadow mt-5 table-auto">
+          <thead className="bg-blue-800 text-white">
+            <tr>
+              <th className="p-2">Cliente</th>
+              <th className="p-2">Contactos</th>
+              <th className="p-2">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {clientes.map((cliente) => (
+              <Cliente cliente={cliente} key={cliente.id} />
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p className="text-center mt-10">No ahy Clientes aun</p>
+      )}
     </>
   );
 };
