c35fe90b405cb5cb1fd9514e87488521a72d456e
Ulises Sandoval <ulises.sandoval@uabc.edu.mx>
2022-10-07 10:12:48 -0700

    Primeros Pasos creando el Formulario de Clientes

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,3 +1,23 @@
+import { useNavigate } from "react-router-dom";
+
 export const NuevoCliente = () => {
-  return <div>NuevoCliente</div>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <h1 className="font-blanck text-4xl text-blue-900">Nuevo Cliente</h1>
+      <p>Llena todos los campos para registrar un nuevo cliente </p>
+
+      <div className="flex justify-end">
+        <button
+          className="bg-blue-800 text-white px-3 py-1 font-bold uppercase"
+          onClick={() => navigate(-1)}
+        >
+          VOLVER
+        </button>
+      </div>
+      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10">
+        <p>Formulario aqui</p>
+      </div>
+    </>
+  );
 };
