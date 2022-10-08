bb2545bd9e7cf3c430f98f6fca18f03f95007bd2
Ulises Sandoval <ulises.sandoval@uabc.edu.mx>
2022-10-07 17:49:05 -0700

    Añadiendo el Formulario para Editar y Autocompletar el Formulario

--- src/pages/EditarCliente.jsx
+++ src/pages/EditarCliente.jsx
@@ -1,4 +1,6 @@
+import { Form, useNavigate, useLoaderData } from "react-router-dom";
 import { obtenerCliente } from "../data/clientes";
+import Formulario from "../components/Formulario";
 
 export async function loader({ params }) {
   const cliente = await obtenerCliente(params.clienteId);
@@ -8,9 +10,37 @@ export async function loader({ params }) {
       statusText: "No ahi resultados",
     });
   }
-  console.log(cliente);
+  return cliente;
 }
 
 export const EditarCliente = () => {
-  return <div>EditarCliente</div>;
+  const navigate = useNavigate();
+  const cliente = useLoaderData();
+  return (
+    <>
+      <h1 className="font-blanck text-4xl text-blue-900">Editar Cliente</h1>
+      <p>Llena todos los campos para registrar un nuevo cliente </p>
+
+      <div className="flex justify-end">
+        <button
+          className="bg-blue-800 text-white px-3 py-1 font-bold uppercase"
+          onClick={() => navigate(-1)}
+        >
+          VOLVER
+        </button>
+      </div>
+      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
+        {/* {errores?.length &&
+          errores.map((error, i) => <Error key={i}>{error}</Error>)} */}
+        <Form noValidate method="post">
+          <Formulario cliente={cliente} />
+          <input
+            type="submit"
+            className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg"
+            value="Registrar Cliente"
+          />
+        </Form>
+      </div>
+    </>
+  );
 };
